9a59bd00d23c7ac2833a0ff53b5fb050dc2e4bbc
Elizabeth Lovero <elizabethlovero@Elizabeths-MacBook-Pro.local>
2024-03-07 21:02:23 -0600

    feat: functioning scraper but is redundant.

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,21 @@
 class EntriesController < ApplicationController
+  def create 
+    result = CreateEntry.new.call(entry_params)
+    if result.success?
+      redirect_to channel_path(result.channel)
+    else
+      redirect_to channel_path(result.channel), alert: result.error
+    end
 end
+
+class RemoteEntries
+  def call(url)
+    feed = Feedjira::Feed.fetch_and_parse(url)
+    # feed.entries.map do |entry|
+    #   {
+    #     title: entry.title,
+    #     published: entry.published,
+    #     content: entry.content,
+    #     url: entry.url,
+    # }
+    end
